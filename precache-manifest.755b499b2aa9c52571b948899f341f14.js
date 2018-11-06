f5bfe7828b3481eb4bd6d34fa42e2a73cb5af57d
Edson Yuity Yonezawa Caldeira <edson@FORLOGIC.LOCAL>
2018-11-06 18:13:38 -0200

    Updates

---
+++ precache-manifest.755b499b2aa9c52571b948899f341f14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "ae5f7f831574cf68cf14",
+    "url": "/profile/static/js/main.ae5f7f83.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/profile/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ae5f7f831574cf68cf14",
+    "url": "/profile/static/css/main.47458067.chunk.css"
+  },
+  {
+    "revision": "e8b28d9b24acfd29c6ea3ba22800787e",
+    "url": "/profile/index.html"
+  }
+];
